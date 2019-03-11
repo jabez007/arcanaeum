b44a4f2e9f255c54d61da7ba8a57e7ec796e1614
Jimmy McCann <jabez.m.007@gmail.com>
2019-03-10 20:13:59 -0500

    add vue-router

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import './plugins/vuetify'
 import Babylon from 'vue-babylonjs';
 import App from './App.vue';
+import router from './router'
 
 Vue.use(Babylon);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <Scene id="canvas"
+           ref="scene"
+           fog="exp"
+           :fogDensity="0.01"
+           v-model="scene">
+      <Camera v-model="camera"
+              type="arcRotate" 
+              :radius="20" 
+              :position="[0, 0, -25]">
+      </Camera>
+      <HemisphericLight diffuse="#512da8"></HemisphericLight>
+      <RotatingEntity :duration="getRandomInt(30 ,60)">
+        <PointLight diffuse="#03a9f4"></PointLight>
+        <RotatingEntity v-for="(box, key) in boxes"
+                        :key="key"
+                        :position="box.pos"
+                        :duration="getRandomInt(5, 15)">
+          <Box v-model="nodes[key]">
+            <Material :alpha="0.8">
+            </Material>
+          </Box>
+        </RotatingEntity>
+      </RotatingEntity>
+    </Scene>
+    <div ref="tooltip"
+         id="tooltip">
+    </div>    
+  </div>
+</template>
+
+<script>
+const BOXES = [
+  {
+    name: 'hello',
+    pos: [4, 0, 0],
+    to: '/hello'
+  },
+  {
+    name: 'world',
+    pos: [0, 4, 0],
+    to: '/world'
+  },
+  {
+    name: 'foo',
+    pos: [0, 0, 4],
+    to: '/foo'
+  },
+  {
+    name: 'bar',
+    pos: [-4, 0, 0],
+    to: '/bar'
+  },
+  {
+    name: 'good',
+    pos: [0, -4, 0],
+    to: '/good'
+  },
+  {
+    name: 'bye',
+    pos: [0, 0, -4],
+    to: '/bye'
+  },
+]; 
+
+export default {
+  name: 'home',
+  components: {
+    RotatingEntity: () => import('@/components/RotatingEntity'),
+  },
+  data: () => ({
+    scene: null,
+    camera: null,
+    nodes: [],
+  }),
+  computed: {
+    boxes() {
+      return BOXES;
+    }
+  },
+  watch: {
+    scene() {
+      // console.log('scene ready');
+    },
+    camera() {
+      // console.log('camera ready');
+      // cheat scene in here
+      this.scene = this.scene || this.camera._scene;
+    },
+  },
+  mounted() {
+    window.removeEventListener('click', this.pickMesh);
+    window.addEventListener('click', this.pickMesh);
+    window.removeEventListener('mousemove', this.showTooltip);
+    window.addEventListener('mousemove', this.showTooltip);
+  },
+  methods: {
+    getRandomInt(min, max) {
+      min = Math.ceil(Math.min(min, max));
+      max = Math.floor(Math.max(min, max));
+      // dont give a  0
+      const rand = Math.floor(Math.random() * (max - min)) + min;  // The maximum is exclusive and the minimum is inclusive
+      if (rand == 0) {
+          return this.getRandomInt(min, max);
+      } else {
+          return rand ; 
+      }
+    },
+    showTooltip() {
+      const box = this.pickMesh();
+      if (box.name) {
+        this.$refs.tooltip.style.background = '#D1C4E9';
+        this.$refs.tooltip.style.border = '1px solid #673ab7'
+        this.$refs.tooltip.innerText = box.name;
+      } else {
+        this.$refs.tooltip.style.background = 'transparent';
+        this.$refs.tooltip.style.border = '';
+        this.$refs.tooltip.innerText = '';
+      }
+    },
+    pickMesh() {
+      const pickResult = this.scene.pick(this.scene.pointerX, this.scene.pointerY);
+      if (pickResult.hit) {
+        const index = this.nodes.findIndex(n => n.id === pickResult.pickedMesh.id);
+        if (index >= 0) {
+          return BOXES[index];
+        }
+      }
+      return {};
+    }
+  }
+};
+</script>
+
+<style scoped>
+#tooltip {
+  position: absolute;
+  top: 50%;
+  right: 45%;
+  min-width: 10%;
+  border-radius: 25px;
+  opacity: 0.5;
+  color: #212121;
+  font-size: large;
+  font-family: Aclonica, Merienda, 'Lucida Sans Unicode', 'Lucida Grande', sans-serif;
+  text-align: center;
+  padding: 5px;
+  margin: 0 auto;
+}
+
+/*#canvas {
+  background-image: url('./assets/banner.jpg') !important;
+}*/
+</style>
