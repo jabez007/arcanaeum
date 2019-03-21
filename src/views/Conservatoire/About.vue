69247ae13d283baef4067c7db5041b5ed8758875
Jimmy <jabez007@users.noreply.github.com>
2019-03-21 10:43:27 -0500

    Create Conservatoire About.vue

---
+++ src/views/Conservatoire/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card class="text-xs-center">
+    <img alt="Conservatoire logo" src="../../assets/Conservatoire/logo.png">
+    <v-card-title>
+        <v-spacer></v-spacer>
+        <h2 class="display-3">Welcome to the Conservatoire</h2>
+        <v-spacer></v-spacer>
+    </v-card-title>
+    <v-card-text>
+        <p class="subheading">
+            The Conservatoire is the Archons' repository of all knowledge revealed to the Archons through our various explorations and ventures into the Arcane
+        </p>
+        <p>
+           These discoveries are at the disposal of the Free Peoples of Mardrun for the sake and benefit of Mardrun.
+        </p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ConservatoireAbout',
+};
+</script>
