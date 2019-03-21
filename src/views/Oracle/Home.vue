45ab30ee96c17f0bb2838c6e5d69d480dc2fe211
Jimmy <jabez007@users.noreply.github.com>
2019-03-21 09:48:40 -0500

    Create Oracle Home.vue

---
+++ src/views/Oracle/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <AppLayout>
+        <span slot="toolbarTitle">Oracle of the Archons</span>
+    </AppLayout>
+</template>
+
+<script>
+export default {
+    name: 'Oracle',
+    components: {
+        AppLayout: () => import('@/components/AppLayout.vue'),
+    },
+};
+</script>
