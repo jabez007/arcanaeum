b5ecddcaaa0d711e63adf12d5bbfb56c28609485
Jimmy McCann <jabez.m.007@gmail.com>
2019-03-22 10:14:46 -0500

    embed Google slides

--- src/views/Conservatoire/ProjectAllons-y.vue
+++ src/views/Conservatoire/ProjectAllons-y.vue
@@ -1,6 +1,12 @@
 <template>
-  <v-card>
-  </v-card>
+  <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSs4CybXJkMEXzmWltb0fVirM0D1IidquHBBHwH9QChVmQhjBJ0ws81OFIyeqHBcUYvuFoxt19pLwai/embed?start=true&loop=true&delayms=60000" 
+          frameborder="0" 
+          width="100%"
+          height="100%" 
+          allowfullscreen="true" 
+          mozallowfullscreen="true" 
+          webkitallowfullscreen="true">
+  </iframe>
 </template>
 
 <script>
