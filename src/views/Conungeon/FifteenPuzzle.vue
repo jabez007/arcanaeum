58a558e863fc14844c99bc05aa14b21d20f21cb6
Jimmy <jabez007@users.noreply.github.com>
2019-04-08 12:29:25 -0500

    fix fillPatternScale

--- src/views/Conungeon/FifteenPuzzle.vue
+++ src/views/Conungeon/FifteenPuzzle.vue
@@ -38,7 +38,7 @@
                   height: konvaGroupWidth * 4,
                   cornerRadius: konvaGroupWidth / 3, 
                   fillPatternImage = backgroundImage,
-                  fillScale: { x: (konvaGroupWidth * 4) / 1080, y: (konvaGroupWidth * 4) / 1080 }, 
+                  fillPatternScale: { x: (konvaGroupWidth * 4) / 1080, y: (konvaGroupWidth * 4) / 1080 }, 
               }"
             ></v-rect>
           </v-layer>
