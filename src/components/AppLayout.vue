a63f80045dff0146e7c756b65824cdc555c1a60b
Jimmy McCann <jabez.m.007@gmail.com>
2019-04-14 11:42:50 -0500

    add bug report link to app layout

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -1,80 +1,69 @@
 <template>
-    <v-app dark>
-        <v-toolbar app
-                   fixed
-                   clipped-left>
-            <v-toolbar-side-icon v-if="navigationDrawer"
-                                 @click.stop="drawer = !drawer">
-            </v-toolbar-side-icon>
-            <v-toolbar-title>
-                <slot name="toolbarTitle">
-                </slot>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-                <slot name="toolbarItems">
-                </slot>
-            </v-toolbar-items>
-            <v-btn to="/"
-                   icon>
-                <v-icon>local_library</v-icon>
-            </v-btn>
-        </v-toolbar>
-        <v-navigation-drawer v-if="navigationDrawer"
-                             v-model="drawer"
-                             app
-                             fixed
-                             clipped>
-            <slot name="navigationDrawerList">
-            </slot>
-        </v-navigation-drawer>
-        <v-content>
-            <v-container fluid
-                         fill-height>
-                <v-layout justify-center
-                          align-center>
-                    <slot name="content">
-                        <transition :name="transitionName"
-                                    mode="out-in">
-                            <router-view />
-                        </transition>
-                    </slot>
-                </v-layout>
-            </v-container>
-        </v-content>
-        <v-footer class="pa-3"
-                  app
-                  dark>
-            <v-spacer></v-spacer>
-            &copy; {{ new Date().getFullYear() }}
-        </v-footer>
-    </v-app>
+  <v-app dark>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon v-if="navigationDrawer" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <slot name="toolbarTitle"></slot>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <slot name="toolbarItems"></slot>
+      </v-toolbar-items>
+      <v-btn to="/" icon>
+        <v-icon>local_library</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer v-if="navigationDrawer" v-model="drawer" app fixed clipped>
+      <slot name="navigationDrawerList"></slot>
+    </v-navigation-drawer>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <slot name="content">
+            <transition :name="transitionName" mode="out-in">
+              <router-view/>
+            </transition>
+          </slot>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer class="pa-3" app dark>
+      <v-btn href="https://github.com/jabez007/arcanaeum/issues" icon small target="_blank">
+        <v-icon small>bug_report</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      &copy; {{ new Date().getFullYear() }}
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'AppLayout',
+  name: "AppLayout",
   data: () => ({
     drawer: true,
-    transitionName: 'fade-transition',
+    transitionName: "fade-transition"
   }),
   computed: {
     navigationDrawer() {
       return !!this.$slots.navigationDrawerList;
-    },
+    }
   },
   watch: {
     $route(to, from) {
-      const toDepth = to.path.split('/').length;
-      const fromDepth = from.path.split('/').length;
-      if (toDepth < fromDepth) { // moving up
-        this.transitionName = 'slide-y-reverse-transition';
-      } else if (toDepth > fromDepth) { // moving down
-        this.transitionName = 'slide-y-transition';
-      } else { // moving laterally
-        this.transitionName = 'slide-x-transition';
+      const toDepth = to.path.split("/").length;
+      const fromDepth = from.path.split("/").length;
+      if (toDepth < fromDepth) {
+        // moving up
+        this.transitionName = "slide-y-reverse-transition";
+      } else if (toDepth > fromDepth) {
+        // moving down
+        this.transitionName = "slide-y-transition";
+      } else {
+        // moving laterally
+        this.transitionName = "slide-x-transition";
       }
-    },
-  },
+    }
+  }
 };
 </script>
