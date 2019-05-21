9475cbea937dbb899337673e33ce00330e77463b
Jimmy McCann <jabez.m.007@gmail.com>
2019-05-21 16:16:05 -0500

    columnar keyword input

--- src/views/CryptoTron/ColumnarTranspositionCipher.vue
+++ src/views/CryptoTron/ColumnarTranspositionCipher.vue
@@ -24,9 +24,9 @@
               in an irregular columnar transposition cipher, the spaces are left blank.
               Finally, the message is read off in columns, in the order specified by the keyword.
           </p>
+          <h6 class="title">Example</h6>
           <p>
-              <h6 class="title">Example</h6>
-               Suppose we use the keyword <q>ZEBRAS</q> and the message <q>WE ARE DISCOVERED. FLEE AT ONCE.</q>
+            Suppose we use the keyword <a @click="keyword = 'zebras'">ZEBRAS</a> and the message <q>WE ARE DISCOVERED. FLEE AT ONCE.</q>
           </p>
           <p>
               In a regular columnar transposition,
@@ -35,13 +35,31 @@
               In an irregular columnar transposition,
           </p>
           <p>
-              Although weak on its own, it can be combined with other ciphers, such as a substitution cipher,
+              Although stronger than the Rail-Fence cipher, it is still considered weak on its own, 
+              but it can be combined with other ciphers, such as a substitution cipher;
               the combination of which can be more difficult to break than either cipher on it's own.
-              The ADFGVX cipher uses a columnar transposition to greatly improve its security.
+              For example, the ADFGVX cipher also uses a columnar transposition to greatly improve its security.
           </p>
       </v-card-text>
     </v-card>
-    <v-flex slot="key"></v-flex>
+    <v-flex slot="key" xs9>
+      <v-form ref="columnarKeyForm"
+              v-model="keyIsValid">
+        <v-text-field 
+          label="Key Word"
+          v-model.trim="keyword"
+          :rules="[rules.required, rules. min, rules.word]"
+          required
+          clearable
+        ></v-text-field>
+        <v-text-field
+          label="Sorted"
+          :value="(keyword || '').toLowerCase().split('').sort().join('')"
+          disabled
+          readonly
+        ></v-text-field>
+      </v-form>
+    </v-flex>
   </Cipher>
 </template>
 
@@ -53,7 +71,15 @@ export default {
   components: {
     Cipher,
   },
-  data: () => ({}),
+  data: () => ({
+    keyword: '',
+    rules: {
+      required: value => !!value || 'this item is required',
+      word: value => !((value || '').toLowerCase().trim().replace(/[a-z]/g, '')) || 'The key word must be a word',
+      min: value => (value || '').length > 1 || 'The key word should be longer than one character'
+    },
+    keyIsValid: false,
+  }),
   methods: {
     encrypt(plainText, cipherKey) {},
     decrypt(cipherText, cipherKey) {},
