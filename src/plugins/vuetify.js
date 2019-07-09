8be6b7f03ef9199339fe88f97642b2646653a057
Jimmy <jabez007@users.noreply.github.com>
2019-07-09 13:42:53 -0500

    comment explaining property

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,6 +5,15 @@ import 'vuetify/src/stylus/app.styl';
 Vue.use(Vuetify, {
   iconfont: 'md',
   options: {
+    /*
+     * will generate a css variable for each theme color, which can then use in a component's style.
+     *
+     * ex: 
+     *    .something {
+     *      color: var(--v-primary-base)
+     *      background-color: var(--v-accent-lighten2)
+     *    }
+     */
     customProperties: true,
   },
 });
