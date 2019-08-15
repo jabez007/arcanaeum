9f931ff6c85a5a8a716bddc72d112f020b7e9256
Jimmy McCann <jabez.m.007@gmail.com>
2019-08-15 08:57:12 -0500

    fix entry errors

--- src/assets/Conservatoire/Journal/index.js
+++ src/assets/Conservatoire/Journal/index.js
@@ -1,5 +1,6 @@
 export default [...new Array(19).keys()]
   .map(e => ({
     title: `Entry ${e + 1}`,
-    contents: require(`entry${e + 1}`);
+    // eslint-disable-next-line
+    contents: require(`./entry${e + 1}`),
   }));
