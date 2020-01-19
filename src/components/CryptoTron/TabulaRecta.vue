e9f4a57a32c397efe962b8683e6ea065081eadd6
Jimmy McCann <jabez.m.007@gmail.com>
2020-01-19 11:32:49 -0600

    highlight row and column on hover

--- src/components/CryptoTron/TabulaRecta.vue
+++ src/components/CryptoTron/TabulaRecta.vue
@@ -1,19 +1,30 @@
 <template>
-  <v-dialog v-model="dialog" width="740">
+  <v-dialog v-model="dialog" width="760">
     <template v-slot:activator="{ on }">
       <a class="yellow--text text--darken-4" v-on="on">Tabula Recta</a>
     </template>
-    <v-card>
+    <v-card style="z-index: inherit;">
+      <v-layout row>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="dialog = !dialog">
+          <v-icon>close</v-icon>
+        </v-btn>
+      </v-layout>
       <v-card-text>
         <table>
           <thead>
             <th></th>
             <th v-for="i in 26" :key="i">{{ String.fromCharCode(64 + i) }}</th>
           </thead>
-          <tr v-for="j in 26" :key="j">
-            <th style="background: var(--v-secondary-base);">{{ String.fromCharCode(64 + j) }}</th>
-            <td v-for="k in 26" :key="k">{{ String.fromCharCode(65 + (((j - 1) + (k - 1)) % 26)) }}</td>
-          </tr>
+          <tbody>
+            <tr v-for="j in 26" :key="j">
+              <th>{{ String.fromCharCode(64 + j) }}</th>
+              <td
+                v-for="k in 26"
+                :key="k"
+              >{{ String.fromCharCode(65 + (((j - 1) + (k - 1)) % 26)) }}</td>
+            </tr>
+          </tbody>
         </table>
       </v-card-text>
     </v-card>
@@ -61,25 +72,57 @@ export default {
 
 <style scoped>
 table {
-  width: 100%;
-  border-spacing: 1px;
+  border-spacing: 0px;
   border-collapse: separate;
   font-family: monospace, monospace;
+  overflow: hidden;
+  width: 100%;
+}
+td,
+th {
+  position: relative;
+  text-align: center;
+}
+td:hover::after,
+thead th:not(:empty):hover::after,
+td:focus::after,
+thead th:not(:empty):focus::after {
+  content: '';
+  height: 10000px;
+  left: 0;
+  position: absolute;
+  top: -5000px;
+  width: 100%;
+  z-index: -1;
+}
+td:hover::after,
+th:hover::after {
+  background-color: var(--v-primary-base);
 }
 th {
-  text-align: center;
   font-size: larger;
 }
-tr {
-  background: var(--v-primary-base);
-}
+/*
 tr:nth-child(odd) {
   background: var(--v-secondary-lighten1);
 }
 tr:nth-child(even) {
   background: var(--v-secondary-darken1);
 }
-td {
-  text-align: center;
+*/
+tr:hover {
+  background-color: var(--v-primary-base);
+}
+/* Focus stuff for mobile */
+td:focus::before,
+tbody th:focus::before {
+  background-color: var(--v-primary-base);
+  content: '';
+  height: 100%;
+  top: 0;
+  left: -5000px;
+  position: absolute;
+  width: 10000px;
+  z-index: -1;
 }
 </style>
