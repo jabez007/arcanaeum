01e43f11d8795a87ed131759012588a2613bc067
jabez007 <jabez007@users.noreply.github.com>
2025-06-10 15:13:12 -0500

    add google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./assets/main.css";
 
 import { createApp } from "vue";
+import { configure } from "vue-gtag";
 import App from "./App.vue";
 import router from "./router";
 //@ts-expect-error still working on exporting type declarations
@@ -14,6 +15,13 @@ app.use(CryptoTronPlugin, {
 });
 
 console.debug(router.getRoutes());
+
+configure({
+  tagId: "G-6R9GYD8S4K",
+  pageTracker: {
+    router,
+  },
+});
 
 app.use(router);
 
