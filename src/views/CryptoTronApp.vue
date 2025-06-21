2ab29591f753a82491e3bfd4d7678b86f970a548
jabez007 <jabez007@users.noreply.github.com>
2025-06-20 22:21:30 -0500

    updating CryptoTron with new ciphers and builder

--- src/views/CryptoTronApp.vue
+++ src/views/CryptoTronApp.vue
@@ -7,9 +7,15 @@ const CryptoTronAsync = defineAsyncComponent(CryptoTron.app);
 </script>
 
 <template>
-  <CryptoTronAsync style="min-height: 100vh; min-width: 100vw; z-index: 10" />
+  <CryptoTronAsync
+    style="min-height: 100vh; max-height: 100vh; min-width: 100vw; z-index: 10; contain: paint"
+  />
 </template>
 
 <style>
 @import "@jabez007/cryptotron.vue/style.css";
+
+@import "@vue-flow/core/dist/style.css";
+@import "@vue-flow/core/dist/theme-default.css";
+@import "@vue-flow/controls/dist/style.css";
 </style>
